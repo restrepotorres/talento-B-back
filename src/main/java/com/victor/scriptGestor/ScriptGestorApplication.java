d9b714dbf665145ebd8cbc22ecd679195c7e383d
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2024-05-17 13:52:55 -0500

    spring initzlr

---
+++ src/main/java/com/victor/scriptGestor/ScriptGestorApplication.java
@@ -0,0 +1,13 @@
+package com.victor.scriptGestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptGestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/scriptGestor/ScriptGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.scriptGestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
